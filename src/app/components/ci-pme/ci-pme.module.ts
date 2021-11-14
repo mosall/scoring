9e9c33a6d33b4e502587599479956dc924779c27
Mouhamed Wade <mdwade37@gmail.com>
2021-11-14 17:41:55 +0000

    finish identification UI

--- src/app/components/ci-pme/ci-pme.module.ts
+++ src/app/components/ci-pme/ci-pme.module.ts
@@ -4,11 +4,19 @@ import { CommonModule } from '@angular/common';
 import { CiPmeRoutingModule } from './ci-pme-routing.module';
 import { CiPmeComponent } from './ci-pme.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {IdentificationComponent} from "./identification/identification.component";
+import {EligibiliteComponent} from "./eligibilite/eligibilite.component";
+import {AccueilComponent} from "./accueil/accueil.component";
+import {QualitatifComponent} from "./qualitatif/qualitatif.component";
 
 
 @NgModule({
   declarations: [
     CiPmeComponent,
+    IdentificationComponent,
+    EligibiliteComponent,
+    AccueilComponent,
+    QualitatifComponent
   ],
   imports: [
     CommonModule,
--- src/app/components/ci-pme/identification/identification.component.ts
+++ src/app/components/ci-pme/identification/identification.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-identification',
   templateUrl: './identification.component.html',
@@ -10,6 +10,13 @@ export class IdentificationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $('.next-btn').click(() => {
+      $('.nav-pills > .active').next('a').trigger('click');
+    });
+
+    $('.previous-btn').click(() => {
+      $('.nav-pills > .active').prev('a').trigger('click');
+    });
   }
 
 }
