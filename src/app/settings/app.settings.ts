3d44fa6e6b40318d165c7d2a7e5ac43d10233467
msall1006 <mohamaddou.sall@awaconsulting.com>
2021-11-27 11:55:44 +0000

    Refactoring app.setting

--- src/app/settings/app.settings.ts
+++ src/app/settings/app.settings.ts
@@ -1,11 +1,15 @@
 import {HttpHeaders} from "@angular/common/http";
 
 export class AppSettings {
-  public static CIPME_ADMINISTRATION_API_URL    = 'http://localhost:3000/administration';
-  public static CIPME_SCORING_API_URL    = 'http://localhost:4000/scoring';
 
-  public static UM_URL = "http://localhost:3000/administration/api/auth";
-  public static UM_TOKEN_URL = "http://localhost:3000/administration/oauth/token";
+  private static BACK_IP = 'http://217.182.185.176';
+
+
+  public static CIPME_ADMINISTRATION_API_URL    = AppSettings.BACK_IP+':3000/administration';
+  public static CIPME_SCORING_API_URL    = AppSettings.BACK_IP+':4000/scoring';
+
+  public static UM_URL = AppSettings.BACK_IP+":3000/administration/api/auth";
+  public static UM_TOKEN_URL = AppSettings.BACK_IP+":3000/administration/oauth/token";
 
 
   public static httpOptions = {
