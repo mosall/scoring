3b27d392c896086f822ac2542ee83c2fe164018c
Mohamadou SALL <mohamaddou.sall@awaconsulting.com>
2022-05-05 11:22:53 +0000

    buid: set environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,23 @@
+import { HttpHeaders } from "@angular/common/http";
+
+const BACK_IP: string = 'http://217.182.185.176';
+
 export const environment = {
-  production: true
+  production: true,
+
+  CIPME_ADMINISTRATION_API_URL: BACK_IP+':9000/administration',
+  CIPME_SCORING_API_URL: BACK_IP+':8000/scoring',
+
+  UM_URL: BACK_IP+":9000/administration/api/auth",
+  UM_TOKEN_URL: BACK_IP+":9000/administration/oauth/token",
+
+
+  UM_HOME: BACK_IP+"/administration-awi/login",
+
+  httpOptions: {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Access-Control-Allow-Origin': '*'
+    })
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,28 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { HttpHeaders } from "@angular/common/http";
+
+const BACK_IP: string = 'http://localhost';
+
 export const environment = {
-  production: false
+  production: false,
+
+  CIPME_ADMINISTRATION_API_URL: BACK_IP+':3000/administration',
+  CIPME_SCORING_API_URL: BACK_IP+':4000/scoring',
+
+  UM_URL: BACK_IP+":3000/administration/api/auth",
+  UM_TOKEN_URL: BACK_IP+":3000/administration/oauth/token",
+
+
+  UM_HOME: BACK_IP+"/administration-awi/login",
+
+  httpOptions: {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Access-Control-Allow-Origin': '*'
+    })
+  }
 };
 
 /*
