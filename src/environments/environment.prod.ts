f7333b3d2edc5f8df21a006ab826db5a79c1c5a8
Mohamadou SALL <nillhiam@gmail.com>
2022-08-03 17:02:15 +0000

    build: update environment variable for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,11 +5,11 @@ const BACK_IP: string = 'http://217.182.185.176';
 export const environment = {
   production: true,
 
-  CIPME_ADMINISTRATION_API_URL: BACK_IP+':3000/administration',
-  CIPME_SCORING_API_URL: BACK_IP+':4000/scoring',
+  CIPME_ADMINISTRATION_API_URL: BACK_IP+':9000/administration',
+  CIPME_SCORING_API_URL: BACK_IP+':8000/scoring',
 
-  UM_URL: BACK_IP+":3000/administration/api/auth",
-  UM_TOKEN_URL: BACK_IP+":3000/administration/oauth/token",
+  UM_URL: BACK_IP+":9000/administration/api/auth",
+  UM_TOKEN_URL: BACK_IP+":9000/administration/oauth/token",
 
 
   UM_HOME: BACK_IP+"/administration/login",
